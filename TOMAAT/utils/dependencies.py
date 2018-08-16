6831775ec6368d6206fd661cab72b3170684c9af
j-frei <27365662+j-frei@users.noreply.github.com>
2018-08-16 23:02:05 +0200

    Update pip module import for newer Python2 Slicer support. (#11)

--- TOMAAT/utils/dependencies.py
+++ TOMAAT/utils/dependencies.py
@@ -1,15 +1,21 @@
+# import pip main
+try:
+    from pip import main as pipmain
+except:
+    from pip._internal import main as pipmain
+
+# install requests
 try:
   import requests
 except:
-  import pip
-  pip.main(['install','requests'])
+  pipmain(['install','requests'])
   import requests
   pass
 
+# install requests_toolbelt
 try:
   from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 except:
-  import pip
-  pip.main(['install', 'requests_toolbelt'])
+  pipmain(['install', 'requests_toolbelt'])
   from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
   pass
